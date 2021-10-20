12701697a6599128431596d57a06137bcc5f96b4
rafael <rafaelsistem87@gmail.com>
2021-10-20 07:42:21 -0700

    provider Component

--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -1,5 +1,37 @@
-import { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 const UserContext = createContext()
 
+const UserProvider = ({children})=>{
+
+      const initialState = {
+            id:1,
+            name:'miguelito',
+            isFavorite:[1,2,3]
+      }
+
+      const [user, setUser]=useState(initialState)
+
+      const login = () =>{
+            setUser(initialState)
+      }
+
+      const logOut = () =>{
+            setUser(null)
+      }
+
+      const data = {
+            user,
+            login, 
+            logOut
+      }
+
+      return(
+            <UserContext.Provider value={data}>
+                  {children}
+            </UserContext.Provider>
+      )
+}
+
+export {UserProvider}
 export default UserContext
--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,16 @@ import './App.css'
 import React, {useState} from 'react'
 import Navbar from './components/Navbar'
 import MovieList from './components/MovieList'
-import UserContext from '../context/UserContext'
+import { UserProvider } from '../context/UserContext'
+
 
 function App() {
-
-  const initialUser = {
-    id:1,
-    name:'meme',
-    isFavorite: [1,2,3]
-  }
- 
-  const [user, setUser]=useState(initialUser)
-
-  const login = () =>{
-    setUser(initialUser)
-  }
-  
-  const logOut = () =>{
-    setUser(null)
-  }
-
-  const data = {
-    user,
-    login,
-    logOut
-  }
-
   return (
     <div className="App">
-      <UserContext.Provider value={data}>
+      <UserProvider>
         <Navbar />
         <MovieList /> 
-      </UserContext.Provider>  
+      </UserProvider>  
     </div>
   )
 }
